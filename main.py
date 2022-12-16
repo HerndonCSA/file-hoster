e667ce8975da9d1b27dc92defd789a78aa753943
soos <62181284+sooswastaken@users.noreply.github.com>
2022-12-15 22:01:03 -0500

    Added Frontend

--- main.py
+++ main.py
@@ -7,20 +7,6 @@ import os
 
 app = Sanic(__name__)
 
-# create an in-memory cache to store the file data
-cache = {}
-
-
-async def get_file_data(file_path):
-    # check if the file data is already in the cache
-    if file_path in cache:
-        return cache[file_path]
-    # if the file data is not in the cache, read it from the file and store it in the cache
-    async with aiofiles.open(file_path, "rb") as f:
-        data = await f.read()
-        cache[file_path] = data
-        return data
-
 
 @app.route("/upload", methods=["POST"])
 async def upload_file(request):
@@ -44,15 +30,6 @@ async def upload_file(request):
 async def view_file(request, file_name):
     # check if the "embed" query parameter is set to "true"
     embed = request.args.get("embed", "false") == "true"
-    # get the file from the /files directory
-    file_path = os.path.join("files", file_name)
-    # get the file data from the cache or from the file
-    file_data = await get_file_data(file_path)
-    # determine the MIME type of the file based on its extension
-    content_type, _ = mimetypes.guess_type(file_path)
-    # set the Content-Type header to the determined MIME type
-    headers = {"Content-Type": content_type}
-    # if the "embed" query parameter is set to "true", return the file data as HTML with meta tags
     if embed:
         image_url = request.url.replace("?embed=true", "")
         # to embed on discord and twitter, send html response with meta tags
@@ -61,8 +38,10 @@ async def view_file(request, file_name):
             f"""
             <html>
                 <head>
-                    <meta property="og:title" content="{image_url}" />
-                    <meta name="twitter:image:src" content="{image_url}" />
+                    <meta content="File Hoster v2" property="og:site_name">
+                    <meta property="og:title" content="{file_name}" />
+                    <meta property="og:image" content="{image_url}" />
+                    <meta name="twitter:card" content="summary_large_image">
                 </head>
                 <body>
                     <img src="{image_url}" />
@@ -71,7 +50,7 @@ async def view_file(request, file_name):
             """
         )
     # if the "embed" query parameter is not set or is set to "false", return the file data as a response
-    return response.raw(file_data, headers=headers)
+    return await response.file_stream("files/" + file_name)
 
 
 @app.route("/files")
