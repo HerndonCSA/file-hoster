5cf51f7a9dfde9b7e6e8ad7b8206246d43357b49
soos <62181284+sooswastaken@users.noreply.github.com>
2022-12-17 21:33:34 -0500

    Bruh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import {useEffect, useState} from 'react'
-import './App.css'
 
-const API_URL = 'http://localhost:8000'
+const API_URL = 'https://7a7b-2a09-bac1-7681-62b0-00-10-46b.ngrok.io'
 
 
 function App() {
@@ -16,7 +15,7 @@ function App() {
             })
     }, [])
 
-    return (<div className="App"
+    return (<div className="app"
                  onDrop={e => {
                      e.preventDefault()
                      const file = e.dataTransfer.files
@@ -46,11 +45,71 @@ function App() {
     >
         <div className="flex">
             <h1>Drag an image to upload</h1>
+            <button
+                onClick={() => {
+                    // upload images from clients file system
+                    const input = document.createElement('input')
+                    // only allow images
+                    input.setAttribute('type', 'file')
+                    input.setAttribute('accept', 'image/*')
+
+                    input.type = 'file'
+                    input.multiple = true
+                    input.onchange = e => {
+                        const files = e.target.files
+                        for (let i = 0; i < files.length; i++) {
+                            const formData = new FormData()
+                            formData.append('file', files[i])
+                            fetch(`${API_URL}/upload`, {
+                                method: 'POST', body: formData
+                            })
+                                .then(response => response.json())
+                                .then(data => {
+                                    console.log(data)
+                                    setFiles(data['files'])
+                                })
+                        }
+                    }
+                    input.click()
+                }}
+            > or press to upload
+            </button>
         </div>
         <div className="images">
-            {files.map(file => (<div key={file}>
-                <img src={`${API_URL}/view/${file}`} alt={file}/>
-            </div>))}
+            {files.map(file => {
+                if (2 ) return (<div key={file}>
+                        <div className="image">
+                            <img src={`${API_URL}/view/${file}`} alt={file}/>
+                            <div className="action-bar">
+                                <h1>{file}</h1>
+                                <button
+                                    onClick={() => {
+                                        fetch(`${API_URL}/delete/${file}`, {
+                                            method: 'DELETE'
+                                        })
+                                            .then(response => response.json())
+                                            .then(data => {
+                                                console.log(data)
+                                                setFiles(data['files'])
+                                            })
+                                    }}
+                                >Delete
+                                </button>
+                                <button
+                                    onClick={() => {
+                                        // copy image url to clipboard
+                                        navigator.clipboard.writeText(`${API_URL}/view/${file}?embed=true`).then(() => {
+                                            alert('Copied to clipboard')
+                                        }
+                                        )
+                                    }}
+                                >Embed
+                                </button>
+                            </div>
+                        </div>
+                    <img src={`${API_URL}/view/${file}`} alt={file}/>
+                </div>)
+            })}
         </div>
     </div>)
 }
--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from sanic import Sanic
 from sanic import response
 import aiofiles
 import os
-from sanic_cors import CORS
 
+import subprocess
+
+from sanic_cors import CORS
 
 app = Sanic(__name__)
 CORS(app)
 
 
-@app.route("/upload", methods=["POST"])
+
+@app.route("/upload", methods=["POST", "GET"])
 async def upload_file(request):
+    print(request.files)
     # get the file from the request
     file = request.files.get("file")
 
@@ -21,7 +25,7 @@ async def upload_file(request):
         os.makedirs("files")
 
     # save the file to the /files directory using aiofiles
-    file_path = os.path.join("files", file.name)
+    file_path = os.path.join("files", file.name.replace('%20', ' '))
     async with aiofiles.open(file_path, "wb") as f:
         await f.write(file.body)
 
@@ -32,29 +36,63 @@ async def upload_file(request):
 
 @app.route("/view/<file_name>")
 async def view_file(request, file_name):
+    # remove %20 from file_name
+    file_name = file_name.replace("%20", " ")
     # check if the "embed" query parameter is set to "true"
     embed = request.args.get("embed", "false") == "true"
     if embed:
         image_url = request.url.replace("?embed=true", "")
         # to embed on discord and twitter, send html response with meta tags
         # to link to the file, use the /view/<file_name> endpoint without the "embed" query parameter
-        return response.html(
-            f"""
-            <html>
-                <head>
-                    <meta content="File Hoster v2" property="og:site_name">
-                    <meta property="og:title" content="{file_name}" />
-                    <meta property="og:image" content="{image_url}" />
-                    <meta name="twitter:card" content="summary_large_image">
-                </head>
-                <body>
-                    <img src="{image_url}" />
-                </body>
-            </html>
-            """
-        )
+
+        # if .png, .jpg, or .jpeg, use <meta property="og:image" content="https://example.com/image.png">
+        if file_name.endswith(('.png', '.jpg', '.jpeg')):
+            return response.html(
+                f"""
+                        <html>
+                            <head>
+                                <meta content="File Hoster v2" property="og:site_name">
+                                <meta property="og:title" content="{file_name}" />
+                                <meta property="og:image" content="{image_url}" />
+                                <meta name="twitter:card" content="summary_large_image">
+                            </head>
+                            <body>
+                                <img src="{image_url}" />
+                            </body>
+                        </html>
+                        """
+            )
+        # if .mp4, .mov, or .webm, use <meta property="og:video" content="https://example.com/video.mp4">
+        elif file_name.endswith(('.mp4', '.mov', '.webm')):
+            return response.html(
+                f"""
+                        <html>
+                            <head>
+                <meta name="twitter:title" content="{file_name}">
+                <meta name="twitter:card" content="summary_large_image">
+                <meta property="og:video:url" content="{image_url}">
+                <meta property="og:video:height" content="720">
+                <meta property="og:video:width" content="1280">
+                <meta property="og:type" content="video.other">
+                            </head>
+                            <body>
+                                <video controls src="{image_url}" />
+                            </body>
+                        </html>
+                        """
+            )
     # if the "embed" query parameter is not set or is set to "false", return the file data as a response
     return await response.file_stream("files/" + file_name)
+
+
+@app.route("/delete/<file_name>", methods=["DELETE"])
+async def delete_file(request, file_name):
+    # delete the file from the /files directory
+    os.remove("files/" + file_name)
+
+    # return a JSON response indicating that the file was successfully deleted
+    files = os.listdir("files")
+    return response.json({"success": True, "files": files})
 
 
 @app.route("/files")
@@ -66,4 +104,4 @@ async def list_files(request):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
