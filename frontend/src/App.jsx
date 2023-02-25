a44c1233262b4250d58a0f76eef7d4e693150fe7
soos <62181284+sooswastaken@users.noreply.github.com>
2023-02-25 13:47:46 -0500

    Stable Release

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,75 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
+import Button from '@mui/material/Button';
+import DeleteIcon from './assets/delete.svg'
+import DownloadIcon from './assets/download.svg'
 
-const API_URL = 'https://7a7b-2a09-bac1-7681-62b0-00-10-46b.ngrok.io'
+
+const API_URL = 'http://localhost:8000'
 
 
 function App() {
     const [files, setFiles] = useState([])
+
+
+    const downloadFile = (file) => {
+        fetch(`${API_URL}/view/${file}`)
+            .then(response => response.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob)
+                const a = document.createElement('a')
+                a.style.display = 'none'
+                // set the href to the blob url
+                a.href = url
+                // set the download attribute to the file name
+                a.setAttribute('download', file)
+                // append the anchor tag to the body
+                document.body.appendChild(a)
+                // click the anchor tag to download the file
+                a.click()
+                // remove the anchor tag from the body
+                document.body.removeChild(a)
+            })
+    }
+
+    const deleteFile = (file) => {
+        console.log("deleting file" + file)
+        fetch(`${API_URL}/delete/${file}`, {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setFiles(data['files'])
+                console.log("deleted file " + file + " " + (data["files"].includes(file) === false))
+            })
+    }
+
+
+    const uploadFile = () => {
+        // upload images from clients file system
+        const input = document.createElement('input')
+        // only allow images
+        input.setAttribute('type', 'file')
+        input.type = 'file'
+        input.multiple = true
+        input.onchange = e => {
+            const files = e.target.files
+            for (let i = 0; i < files.length; i++) {
+                const formData = new FormData()
+                formData.append('file', files[i])
+                fetch(`${API_URL}/upload`, {
+                    method: 'POST', body: formData
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        setFiles(data['files'])
+                    })
+            }
+        }
+        input.click()
+    }
+
 
     useEffect(() => {
         fetch(`${API_URL}/files`)
@@ -15,103 +80,82 @@ function App() {
             })
     }, [])
 
-    return (<div className="app"
-                 onDrop={e => {
-                     e.preventDefault()
-                     const file = e.dataTransfer.files
-                     for (let i = 0; i < file.length; i++) {
-                         const formData = new FormData()
-                         formData.append('file', file[i])
-                         fetch(`${API_URL}/upload`, {
-                             method: 'POST', body: formData
-                         })
-                             .then(response => response.json())
-                             .then(data => {
-                                 console.log(data)
-                                 setFiles(data['files'])
-                             })
-                     }
+    useEffect(() => {
+        console.log("adding event listeners")
+        const handleDrop = e => {
+            e.preventDefault()
+            const file = e.dataTransfer.files
+            for (let i = 0; i < file.length; i++) {
+                const formData = new FormData()
+                formData.append('file', file[i])
+                fetch(`${API_URL}/upload`, {
+                    method: 'POST', body: formData
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        setFiles(data['files'])
+                    })
+            }
+        }
 
-                 }}
-                 onDragOver={e => e.preventDefault()}
-                 onDragEnter={e => e.preventDefault()}
-                 onDragLeave={e => e.preventDefault()}
-                 onDragEnd={e => e.preventDefault()}
-                 onDragExit={e => e.preventDefault()}
-                 onDragStart={e => e.preventDefault()}
-                 onDrag={e => e.preventDefault()}
+        const handleDragOver = e => {
+            e.preventDefault()
+        }
+
+        document.getElementById("root").addEventListener('drop', handleDrop)
+        document.getElementById("root").addEventListener('dragover', handleDragOver)
+
+        return () => {
+            document.getElementById("root").removeEventListener('drop', handleDrop)
+            document.getElementById("root").removeEventListener('dragover', handleDragOver)
+        }
+    }, [])
 
 
-    >
-        <div className="flex">
-            <h1>Drag an image to upload</h1>
-            <button
-                onClick={() => {
-                    // upload images from clients file system
-                    const input = document.createElement('input')
-                    // only allow images
-                    input.setAttribute('type', 'file')
-                    input.setAttribute('accept', 'image/*')
 
-                    input.type = 'file'
-                    input.multiple = true
-                    input.onchange = e => {
-                        const files = e.target.files
-                        for (let i = 0; i < files.length; i++) {
-                            const formData = new FormData()
-                            formData.append('file', files[i])
-                            fetch(`${API_URL}/upload`, {
-                                method: 'POST', body: formData
-                            })
-                                .then(response => response.json())
-                                .then(data => {
-                                    console.log(data)
-                                    setFiles(data['files'])
-                                })
-                        }
-                    }
-                    input.click()
-                }}
-            > or press to upload
-            </button>
-        </div>
-        <div className="images">
-            {files.map(file => {
-                if (2 ) return (<div key={file}>
-                        <div className="image">
-                            <img src={`${API_URL}/view/${file}`} alt={file}/>
-                            <div className="action-bar">
-                                <h1>{file}</h1>
-                                <button
+
+    return (
+        <>
+            <div className="header">
+                <h1>Drag an file</h1>
+                <h2>or</h2>
+                <Button className="upload" variant='contained'
+                    onClick={uploadFile}
+                >Press to upload
+                </Button>
+            </div>
+            <div className="files">
+                {files.map(file => {
+                    return (
+                        <div className="file" key={file["id"]}>
+                            <div className="image">
+                                <img src={`${API_URL}/view/${file["id"]}?preview=true`} alt={file} />
+                            </div>
+                            <div className="controls">
+                                <p>{file["name"].length > 15 ? file["name"].substring(0, 15) + "..." : file["name"]}</p>
+                                <Button>
+                                    <img src={DownloadIcon} alt="Download" onClick={() => downloadFile(file["id"])} />
+                                </Button>
+                                <Button>
+                                    <img src={DeleteIcon} alt="Delete" onClick={() => deleteFile(file["id"])} />
+                                </Button>
+                                <Button
                                     onClick={() => {
-                                        fetch(`${API_URL}/delete/${file}`, {
-                                            method: 'DELETE'
-                                        })
-                                            .then(response => response.json())
-                                            .then(data => {
-                                                console.log(data)
-                                                setFiles(data['files'])
-                                            })
+                                        navigator.clipboard.writeText(`${API_URL}/view/${file}?embed=true`)
                                     }}
-                                >Delete
-                                </button>
-                                <button
-                                    onClick={() => {
-                                        // copy image url to clipboard
-                                        navigator.clipboard.writeText(`${API_URL}/view/${file}?embed=true`).then(() => {
-                                            alert('Copied to clipboard')
-                                        }
-                                        )
-                                    }}
-                                >Embed
-                                </button>
+                                >
+                                    Embed
+                                </Button>
                             </div>
                         </div>
-                    <img src={`${API_URL}/view/${file}`} alt={file}/>
-                </div>)
-            })}
-        </div>
-    </div>)
+                    )
+                })}
+
+
+
+            </div>
+        </>)
 }
 
 
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from sanic import Sanic
 from sanic import response
 import aiofiles
 import os
-
+import aiosqlite
+import uuid
 import subprocess
 
 from sanic_cors import CORS
@@ -13,25 +14,62 @@ app = Sanic(__name__)
 CORS(app)
 
 
+@app.listener('before_server_start')
+async def init(application, loop):
+    application.ctx.db = await aiosqlite.connect('file_index.db', loop=loop)
+    await app.ctx.db.execute('''
+        CREATE TABLE IF NOT EXISTS file_index (
+            name TEXT,
+            id TEXT,
+            preview TEXT
+        )
+    ''')
+    await application.ctx.db.commit()
+
+
+@app.listener('after_server_stop')
+async def close(application, _):
+    await application.ctx.db.close()
+
+
+app.register_listener(init, 'before_server_start')
+app.register_listener(close, 'after_server_stop')
+
 
 @app.route("/upload", methods=["POST", "GET"])
 async def upload_file(request):
     print(request.files)
     # get the file from the request
     file = request.files.get("file")
-
-    # create the /files directory if it doesn't exist
+    file_name = uuid.uuid4().hex
+    # path of file will be uuid and extension
+    file_path = os.path.join("files", file_name + mimetypes.guess_extension(file.type))
     if not os.path.exists("files"):
         os.makedirs("files")
 
     # save the file to the /files directory using aiofiles
-    file_path = os.path.join("files", file.name.replace('%20', ' '))
     async with aiofiles.open(file_path, "wb") as f:
         await f.write(file.body)
 
-    # return a JSON response indicating that the file was successfully uploaded
-    files = os.listdir("files")
-    return response.json({"success": True, "files": files})
+    # if the file is a video, create a preview image
+    if mimetypes.guess_type(file_path)[0].startswith('video'):
+        subprocess.run(['ffmpeg', '-i', file_path, '-ss', '00:00:01.000', '-vframes', '1', file_path + '.jpg'])
+        await app.ctx.db.execute('''
+            INSERT INTO file_index (name, id, preview) VALUES (?, ?, ?)
+        ''', (file.name, file_path.replace('files/', ''), file_path.replace('files/', '') + '.jpg'))
+    else:
+        await app.ctx.db.execute('''
+            INSERT INTO file_index (name, id) VALUES (?, ?)
+        ''', (file.name, file_path.replace('files/', '')))
+    await app.ctx.db.commit()
+
+    # return a JSON response indicating that the file was successfully uploaded, and a list of all files from the
+    # database
+    files = await app.ctx.db.execute('''
+        SELECT * FROM file_index
+    ''')
+    files = await files.fetchall()
+    return response.json({"success": True, "files": [{"id": file[1], "name": file[0]} for file in files]})
 
 
 @app.route("/view/<file_name>")
@@ -40,6 +78,29 @@ async def view_file(request, file_name):
     file_name = file_name.replace("%20", " ")
     # check if the "embed" query parameter is set to "true"
     embed = request.args.get("embed", "false") == "true"
+    preview = request.args.get("preview", "false") == "true"
+
+    # get from database
+    file = await app.ctx.db.execute('''
+        SELECT * FROM file_index WHERE id = ?
+    ''', (file_name,))
+    file = await file.fetchone()
+    if file is None:
+        return response.json({"success": False, "error": "File not found"})
+    if preview:
+        if file[2] is None:
+            # this file type does not have a preview, as it is not a video
+            # if the file is an image, return the image, otherwise return a question mark image
+
+            if not file_name.endswith(('.png', '.jpg', '.jpeg', "svg", "gif", "bmp", "webp", "avif", "jfif", "webp")):
+                return await response.file_stream("./unknown.png")
+            else:
+                return await response.file_stream(os.path.join("files", file[1]))
+        else:
+            file_name = file[2]
+            print("PREVIEW FILE NAME: " + file_name)
+    else:
+        file_name = file[1]
     if embed:
         image_url = request.url.replace("?embed=true", "")
         # to embed on discord and twitter, send html response with meta tags
@@ -87,20 +148,53 @@ async def view_file(request, file_name):
 
 @app.route("/delete/<file_name>", methods=["DELETE"])
 async def delete_file(request, file_name):
-    # delete the file from the /files directory
-    os.remove("files/" + file_name)
+    # # delete the file from the /files directory
+    # # remove %20 from file_name
+    # file_name = file_name.replace("%20", " ")
+    # os.remove("files/" + file_name)
+    #
+    # # return a JSON response indicating that the file was successfully deleted
+    # files = os.listdir("files")
+    # return response.json({"success": True, "files": files})
 
+    # get the file from the database
+    file = await app.ctx.db.execute('''
+        SELECT * FROM file_index WHERE id = ?
+    ''', (file_name,))
+    file = await file.fetchone()
+    if file is None:
+        return response.json({"success": False, "error": "File not found"})
+    # delete the file from the /files directory
+    os.remove("files/" + file[1])
+    if file[2] is not None:
+        os.remove("files/" + file[2])
+    # delete the file from the database
+    await app.ctx.db.execute('''
+        DELETE FROM file_index WHERE id = ?
+    ''', (file_name,))
+    await app.ctx.db.commit()
     # return a JSON response indicating that the file was successfully deleted
-    files = os.listdir("files")
-    return response.json({"success": True, "files": files})
+    files = await app.ctx.db.execute('''
+        SELECT * FROM file_index
+    ''')
+    files = await files.fetchall()
+    return response.json({"success": True, "files": [{"id": file[1], "name": file[0]} for file in files]})
 
 
 @app.route("/files")
 async def list_files(request):
-    # get the list of filenames from the /files directory
-    filenames = os.listdir("files")
+    # # get the list of filenames from the /files directory
+    # filenames = os.listdir("files")
+    # # return the list of filenames as a JSON response
+    # return response.json({"files": filenames})
+
+    # get the list of filenames from the database
+    files = await app.ctx.db.execute('''
+        SELECT * FROM file_index
+    ''')
+    files = await files.fetchall()
     # return the list of filenames as a JSON response
-    return response.json({"files": filenames})
+    return response.json({"files": [{"id": file[1], "name": file[0]} for file in files]})
 
 
 if __name__ == "__main__":
